bb28103200b6e477c9f8f2f667137c30396c9007
VoidBehemoth <VoidBehemoth@gmail.com>
2023-12-01 18:05:42 -0700

    Add project files.

---
+++ Mortician/src/Mortician.cs
@@ -0,0 +1,23 @@
+﻿using SML;
+
+namespace Mortician
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+        {
+            Mortician.Utils.Logger.Log(ModInfo.PLUGIN_NAME + " has loaded!");
+        }
+    }
+
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "Mortician";
+
+        public const string PLUGIN_NAME = "Mortician";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
